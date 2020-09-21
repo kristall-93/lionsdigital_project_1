3a35da74212afd2ff40b63981fc1484b0f4a973a
kristall-93 <kristushapaf@yandex.ru>
2020-09-21 16:47:47 +0300

    finised landing page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
-    $('.form_slider').slick({
-        dots: true,
-        slidesToShow: 1,
-                slidesToScroll: 1,
-    });
+
+
 });
+
+$('.form_slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    prevArrow: '<button class="arrow_form_slider arrow_form_slider_prev"><img src="images/arrow-form-slider.svg" alt=""></button>',
+    nextArrow: '<button class="arrow_form_slider arrow_form_slider_next"><img src="images/arrow-form-slider.svg" alt=""></button>',
+});
+
+$('.clients_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<button class="arrow_prev_client"></button>',
+    nextArrow: '<button class="arrow_next_client"><img src="images/arrow-next-client.svg" alt=""></button>',
+    // responsive: [
+    //     {
+    //         breakpoint: 1301,
+    //         settings: {
+    //             arrows: false,
+    //         }
+    //     },         
+    // ]
+}); 
