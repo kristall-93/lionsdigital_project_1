07e582eddf5abd5e34fde61ac9e7c72492f636ba
kristall-93 <kristushapaf@yandex.ru>
2020-09-24 14:37:39 +0300

    added adaptive of 1 stage and edits

--- app/js/main.js
+++ app/js/main.js
@@ -8,25 +8,41 @@ $(document).ready(function () {
         prevArrow: '<button class="arrow_form_slider arrow_form_slider_prev"><img src="images/arrow-form-slider.svg" alt=""></button>',
         nextArrow: '<button class="arrow_form_slider arrow_form_slider_next"><img src="images/arrow-form-slider.svg" alt=""></button>',
     });
-    
+
     $('.clients_slider').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
         prevArrow: '<button class="arrow_prev_client"></button>',
         nextArrow: '<button class="arrow_next_client"><img src="images/arrow-next-client.svg" alt=""></button>',
+        responsive: [{
+            breakpoint: 1760,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 1220,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 740,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        ]
+
     });
-    
+
     $('.continue_free').on('click', function () {
         $.fancybox.close();
         $('.progress_bar').addClass('progress_bar_active');
         move();
-        $('.check_btn').removeClass('check_btn_active');    
+        $('.check_btn').removeClass('check_btn_active');
     });
-    
-    $('.user').on('click', function () {
-        $('.sidebar').addClass('sidebar_active');
-    });
-    
+
     function move() {
         var elem = document.getElementById("greenBar");
         var percent = document.getElementById("percent");
@@ -45,24 +61,47 @@ $(document).ready(function () {
                 $('.check_continue_btn').addClass('check_continue_btn_active');
             } else {
                 width++;
-                elem.style.width = width + '%';          
+                elem.style.width = width + '%';
                 percent.innerHTML = width * 1;
             }
         }
     }
 
-
     $('.notification_close').on('click', function () {
         $('.notify').removeClass('notify_active');
     });
-    
+
     $('.checker_text').on('click', function () {
         $('.checker_text').addClass('checker_text_active');
     });
 
+    $('.menu_btn').on('click', function () {
+        $('.header_line').slideToggle();
+    });
+
+
+    $('.user').on('click', function () {
+        $('.sidebar').addClass('sidebar_active');
+    });
+
+    $(".user").click(function () {
+        $(".sidebar").removeClass('hidden');
+        $(".sidebar").addClass('active');
+        $("body, html").css('overflow', 'hidden');
+        return false;
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $(".sidebar");
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('.sidebar').hasClass('active')) {
+                $(".sidebar").removeClass('active');
+                $(".sidebar").addClass('hidden');
+            }
+            $("body, html").css('overflow', 'auto');
+        }
+    });
+
+    
 });
-
-// $.fancybox({
-//     overlayColor: '#000'
-// });
-
